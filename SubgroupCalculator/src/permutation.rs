97ed9e2370b4f700efa1b45c07bd496f5034e268
Vierkantor <vierkantor@vierkantor.com>
2016-12-14 09:31:24 +0000

    REPORT: commit before compiling

--- SubgroupCalculator/src/permutation.rs
+++ SubgroupCalculator/src/permutation.rs
@@ -10,7 +10,7 @@ pub struct Permutation {
 }
 
 impl fmt::Display for Permutation {
-    fn fmt(&self, f: &fmt::Formatter) -> fmt::Result {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}", self.permutation)
     }
 }
