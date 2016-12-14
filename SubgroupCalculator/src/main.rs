7a3e41d9d2716ad24759e647677b73b77c1955de
Vierkantor <vierkantor@vierkantor.com>
2016-12-14 09:54:03 +0000

    REPORT: commit before compiling

--- SubgroupCalculator/src/main.rs
+++ SubgroupCalculator/src/main.rs
@@ -5,4 +5,6 @@ use permutation::CustomDisplay;
 fn main() {
     let identity = permutation::make_permutation(vec![1, 2, 3, 4, 5]);
     println!("identity: {}", identity.display());
+    let nonsense = permutation::make_permutation(vec![1, 2, 3, 2, 1]);
+    println!("nonsense: {}", nonsense.display());
 }
