6c7324146cf463c558867a32fa41f4c4b8aaf8cf
Vierkantor <vierkantor@vierkantor.com>
2016-12-14 11:05:41 +0000

    REPORT: commit before compiling

--- SubgroupCalculator/src/group.rs
+++ SubgroupCalculator/src/group.rs
@@ -16,9 +16,36 @@ pub struct ConjugacyClass(Subset);
 
 // TODO: check for subgroups
 
+// When the set is a group, wrap it in the Subgroup type.
+pub fn make_subgroup(elements : HashSet<permutation::Permutation>) -> Option<Subgroup> {
+    // First we make it into a Subset by checking the sizes.
+    // For this, we need at least one element.
+    if elements.len() <= 0 {
+        return None;
+    }
+    let &referenceElem = elements.iter().first();
+    let size = referenceElem.permutation.len();
+    for elem in &elements {
+        if elem.permutation.len() != size {
+            return None;
+        }
+    }
+
+    // Then we need to check the group is closed under all operations.
+    for g in &elements {
+        for h in &elements {
+            let tempelem = permutation::composition(g,permutation::invert(h));
+            if !elements.contains(tempelem) {
+                return None;
+            }
+        }
+    }
+}
+
 // Generate the trivial group on the given number of elements.
 pub fn trivial(size : usize) -> Subgroup {
     let mut group = HashSet::new();
     group.insert(permutation::identity(size));
     Subgroup(Subset {size: size, elements: group})
 }
+
