4d73f413391cdba50f81e730fb4615ea8c29161e
Vierkantor <vierkantor@vierkantor.com>
2016-12-14 10:42:59 +0000

    REPORT: commit before compiling

--- SubgroupCalculator/src/group.rs
+++ SubgroupCalculator/src/group.rs
@@ -1,18 +1,21 @@
 use permutation;
 use std::collections::HashSet;
 
+#[derive(Debug)]
 struct Subset {
     order : usize,
     elements : HashSet<permutation::Permutation>,
 }
 
+#[derive(Debug)]
 struct Subgroup(Subset);
+#[derive(Debug)]
 struct ConjugacyClass(Subset);
 
 // TODO: check for subgroups
 
 pub fn trivial(order : usize) -> Subgroup {
-    let mut group = HashSet.new();
+    let mut group = HashSet::new();
     group.insert(permutation::identity(order));
     group
 }
