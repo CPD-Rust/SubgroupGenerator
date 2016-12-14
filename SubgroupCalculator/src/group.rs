4b428d82be8be175678f1cac5f7ecfb7ca71dee1
Vierkantor <vierkantor@vierkantor.com>
2016-12-14 10:47:45 +0000

    REPORT: commit before compiling

--- SubgroupCalculator/src/group.rs
+++ SubgroupCalculator/src/group.rs
@@ -3,7 +3,9 @@ use std::collections::HashSet;
 
 #[derive(Debug)]
 struct Subset {
+    // The number of objects the permutations act on.
     size : usize,
+    // The permutations in the set.
     elements : HashSet<permutation::Permutation>,
 }
 
@@ -14,8 +16,9 @@ struct ConjugacyClass(Subset);
 
 // TODO: check for subgroups
 
-pub fn trivial(order : usize) -> Subgroup {
+// Generate the trivial group on the given number of elements.
+pub fn trivial(size : usize) -> Subgroup {
     let mut group = HashSet::new();
-    group.insert(permutation::identity(order));
-    Subgroup(Subset {size: order, elements: group})
+    group.insert(permutation::identity(size));
+    Subgroup(Subset {size: size, elements: group})
 }
