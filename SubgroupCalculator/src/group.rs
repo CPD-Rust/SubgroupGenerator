5ef38cf60893be1147b275f27d76b9c83922d089
Vierkantor <vierkantor@vierkantor.com>
2016-12-14 10:37:15 +0000

    REPORT: commit before compiling

--- SubgroupCalculator/src/group.rs
+++ SubgroupCalculator/src/group.rs
@@ -10,8 +10,8 @@ struct ConjugacyClass(Subset);
 
 // TODO: check for subgroups
 
-pub fn trivial() : Subgroup {
+pub fn trivial(order : usize) : Subgroup {
     let mut group = HashSet.new();
-    group.insert(permutation::identity());
+    group.insert(permutation::identity(order));
     group
 }
