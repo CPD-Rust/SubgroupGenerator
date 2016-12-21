f8e81acba9e1067f468718eef7291224a41544da
Vierkantor <vierkantor@vierkantor.com>
2016-12-21 11:38:34 +0000

    REPORT: commit before compiling

--- SubgroupCalculator/src/group.rs
+++ SubgroupCalculator/src/group.rs
@@ -2,7 +2,7 @@ use permutation;
 use std::collections::HashSet;
 use std::collections::VecDeque;
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Ord)]
 pub struct Subset {
     // The number of objects the permutations act on.
     size : usize,
@@ -10,7 +10,7 @@ pub struct Subset {
     elements : HashSet<permutation::Permutation>,
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Ord)]
 pub struct Subgroup(Subset);
 #[derive(Debug)]
 pub struct ConjugacyClass(Subset);
